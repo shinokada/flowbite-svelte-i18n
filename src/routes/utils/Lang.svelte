b8f48853f8af2884183ede7d13b702be6523ed5c
Shinichi Okada <okada.shin@gmail.com>
2023-05-19 20:27:09 +0900

    feat: add languages

--- src/routes/utils/Lang.svelte
+++ src/routes/utils/Lang.svelte
@@ -1,46 +1,72 @@
 <script lang="ts">
   import { Dropdown, DropdownItem } from '$lib';
   import { i, language, languages, switchLanguage } from '@inlang/sdk-js'
-  import Us from './Us.svelte';
-  import Ja from './Ja.svelte';
-  async function switchLanguageToEnglish() {
-		console.log('switching language to English ...')
-		await switchLanguage('en')
-		console.log('... language switched to English')
-	}
-  async function switchLanguageToJapanese() {
-		console.log('switching language to Japanese ...')
-		await switchLanguage('ja')
-		console.log('... language switched to Japanese')
-	}
-  let promiseEng = switchLanguageToEnglish();
-  const handleEnglish = ()=>{
-    promiseEng = switchLanguageToEnglish();
+  import De from './flags/De.svelte';
+  import Es from './flags/Es.svelte';
+  import It from './flags/It.svelte';
+  import Ja from './flags/Ja.svelte';
+  import Kr from './flags/Kr.svelte';
+  import No from './flags/No.svelte';
+  import Us from './flags/Us.svelte';
+
+  async function handleLanguageChange(lang: string) {
+    console.log(`switching language to ${lang} ...`)
+    await switchLanguage(lang)
+    console.log(`... language switched to ${lang}`)
   }
-  let promiseJap = switchLanguageToJapanese();
-  const handleJapanese = ()=>{
-    promiseJap = switchLanguageToJapanese();
-  }
+
+  let dropdownOpen = false;
+  const sfLanguages = [
+    {
+      lang: 'de',
+      value: 'Deutsch',
+      icon: De
+    },
+    {
+      lang: 'en',
+      value: 'English',
+      icon: Us
+    },
+    {
+      lang: 'es',
+      value: 'Español',
+      icon: Es
+    },
+    {
+      lang: 'it',
+      value: 'Italiano',
+      icon: It
+    },
+    {
+      lang: 'ja',
+      value: '日本語',
+      icon: Ja
+    },
+    {
+      lang: 'ko',
+      value: '한국어',
+      icon: Kr
+    },
+    {
+      lang: 'no',
+      value: 'Norsk',
+      icon: No
+    },
+  ]
 </script>
 
-<div class='p-2'>
-  <Us size="18" id="countries"/>
+<div class='p-2' id="countries">
+  Lang
 </div>
-<Dropdown triggeredBy="#countries">
-  <DropdownItem on:click={handleEnglish} on:keydown={handleEnglish}>
-    <div class="inline-flex items-center">
-      <Us />
-      <span class='p-2'>
-        English (US)
-      </span>
-    </div>
-  </DropdownItem>
-  <DropdownItem on:click={handleJapanese} on:keydown={handleJapanese}>
-    <div class="inline-flex items-center">
-      <Ja />
-      <span class='p-2'>
-        日本語
-      </span>
-    </div>
-  </DropdownItem>
+<Dropdown triggeredBy="#countries" bind:open={dropdownOpen}>
+  {#each sfLanguages as { lang, value, icon }}
+    <DropdownItem on:click={() => handleLanguageChange(lang)} on:keydown={() => handleLanguageChange(lang)} on:click={() => dropdownOpen = false}>
+      <div class="inline-flex items-center">
+        <svelte:component this={icon} />
+        <span class='p-2'>
+          {value}
+        </span>
+      </div>
+    </DropdownItem>
+  {/each}
 </Dropdown>
